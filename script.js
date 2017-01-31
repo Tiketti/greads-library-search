6e3dc590047a7dd0ef4a03f280844cf4f5746849
Perttu Haliseva <perttu@gmail.com>
2017-01-31 09:35:17 +0200

    Add support for Tampere area library (Piki)

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const _baseLink = 'http://haku.helmet.fi/iii/encore/search/C__S{0}%20{1}__Ff:facetmediatype:1:1:Kirja::__Orightresult__U__X0';
-const _buttonTitle = '[Search Helmet for this book]';
+const libraries = {
+  helsinki: {
+    baseLink: 'http://haku.helmet.fi/iii/encore/search/C__S{0}%20{1}__Ff:facetmediatype:1:1:Kirja::__Orightresult__U__X0',
+    linkTitle: '[Search Helmet for this book]',
+  },
+  tampere: {
+    baseLink: 'https://piki.verkkokirjasto.fi/web/arena/search?p_auth=jHgv3Osu&p_p_id=searchResult_WAR_arenaportlets&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_r_p_687834046_facet_queries=mediaClass_facet%3Dbook&p_r_p_687834046_search_type=solr&p_r_p_687834046_search_query={0}+{1}',
+    linkTitle: '[Search Piki for this book]',
+  },
+};
+const _selectedLibrary = 'helsinki';
 
 function createLinkElement(author, title) {
-  const replacables = /[()*]/g;
-  const sanitizedTitle = title.replace(replacables, '');
-  const sanitizedAuthor = author.replace(replacables, '');
+  const removables = /[()*]/g;
+  const replaceables = /[]/g;
 
-  const searchUri = _baseLink
+  const sanitizedTitle = title.replace(removables, '').replace(replaceables, '+');
+  const sanitizedAuthor = author.replace(removables, '').replace(replaceables, '+');
+
+  const searchUri = libraries[_selectedLibrary].baseLink
     .replace('{0}', sanitizedAuthor)
     .replace('{1}', sanitizedTitle);
 
   let link = document.createElement('a');
   link.setAttribute('href', searchUri);
   link.setAttribute('style', 'font-size: x-small; color:red;');
-  link.innerHTML = _buttonTitle;
+  link.innerHTML = libraries[_selectedLibrary].linkTitle;
 
   return link;
 }
