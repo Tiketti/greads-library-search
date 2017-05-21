f4c4657ce0d7b83ee44330163faddfb09621c23e
Perttu <perttu@gmail.com>
2017-05-21 20:42:55 +0300

    Refactor for more ES6 features, promises Also fortify linting rules

--- options/options.js
+++ options/options.js
@@ -1,41 +1,43 @@
-function saveOptions () {
-  var helsinki = document.getElementById('helsinki').checked;
-  var tampere = document.getElementById('tampere').checked;
-  var helsinkiOverdrive = document.getElementById('helsinkiOverdrive').checked;
+const saveOptions = () => {
+  const helsinki = document.getElementById('helsinki').checked;
+  const tampere = document.getElementById('tampere').checked;
+  const helsinkiOverdrive = document.getElementById('helsinkiOverdrive').checked;
 	
   chrome.storage.sync.set({
     helsinki,
     tampere,
     helsinkiOverdrive,
-  }, function() {
-    var status = document.getElementById('status');
+  }, () => {
+    const status = document.getElementById('status');
     status.textContent = 'Options saved.';
-    setTimeout(function() {
+    setTimeout(() => {
       status.textContent = '';
-		}, 1000 * 3);
+    }, 1000 * 3);
   });
-}
+};
 
-function restoreOptions() {
-	getSelectedLibraries(function (items) {
+const getSelectedLibraries = () => (
+  new Promise((resolve) => {
+    chrome.storage.sync.get({
+      helsinki: true,
+      tampere: false,
+      helsinkiOverdrive: false,
+    }, (items) => {
+      resolve(items);
+    });
+  })
+);  
+
+const restoreOptions = () => {
+  getSelectedLibraries().then((items) => {
     document.getElementById('helsinki').checked = items.helsinki;
     document.getElementById('tampere').checked = items.tampere;
     document.getElementById('helsinkiOverdrive').checked = items.helsinkiOverdrive;
-	});
-}
-
-function getSelectedLibraries(callback) {
-  chrome.storage.sync.get({
-		helsinki: true,
-    tampere: false,
-    helsinkiOverdrive: false,
-	}, function (items) {
-		callback(items);
   });
-}
+};
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-var saveButton = document.getElementById('saveGreadsOptions');
+const saveButton = document.getElementById('saveGreadsOptions');
 if (saveButton) {
-	saveButton.addEventListener('click', saveOptions);
+  saveButton.addEventListener('click', saveOptions);
 }
